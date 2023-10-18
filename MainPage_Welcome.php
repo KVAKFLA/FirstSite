9e868f37324a6b48c803bbc1e9dc711c89e43615
KVAKFLA <prostakyt777@gmail.com>
2023-10-18 22:00:43 +0300

    startfrontend

---
+++ MainPage_Welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel = "shortcut icon" href="Img/icon.png" type="image/x-icon">
+    
+    <link rel="stylesheet" href="MainPage_Welcome.css" />
+    
+    <title>ARTBASE: main page</title>
+
+</head>
+<body>
+    <div class="headpanel">
+        <img src="Img/icon.png" width="5%">
+        <div class="headpanelright">
+            <a>Описание</a>
+            <button type="button" class="button">Аторизация</button>
+        </div>
+    </div>
+</body>
+</html>
